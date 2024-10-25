da5ab7909e6a224afbaffde3db1b7e27f5ecc003
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-10-25 09:36:30 +0300

    up

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,11 @@
 import { Book } from "./Book";
 
 export const BookList = (props) => {
-  const bookElements = [];
-
-  for (const book of props.books) {
-    bookElements.push(
-      <li>
-        <article>
-          <h2>{book.title}</h2>
-          <div>Year: {book.year}</div>
-          <div>Price: {book.price}</div>
-          <footer>
-            <span>Author: {book.author}</span>
-          </footer>
-        </article>
-      </li>
-    )
-  };
+  const bookElements = props.books.map(book => <Book
+    title={book.title}
+    year={book.year}
+    price={book.price}
+    author={book.author} />);
 
   return (
     <ul>
