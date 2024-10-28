6fcff8ec33060baf680cf52cbb213054575d82ff
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-10-28 16:03:21 +0200

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Spinner } from "./components/Spinner";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
@@ -17,95 +19,9 @@ function App() {
 
           <div class="table-wrapper">
 
-            <div class="loading-container">
-              <div class="loading-spinner">
-                <span class="loading-spinner-text">Loading</span>
-              </div>
-            </div>
+            {/*<Spinner />*/}
 
-            <table class="table">
-              <thead>
-                <tr>
-                  <th class="table-header-task">Task</th>
-                  <th class="table-header-status">Status</th>
-                  <th class="table-header-action">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-
-                <tr class="todo is-completed">
-                  <td>Give dog a bath</td>
-                  <td>Complete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo is-completed">
-                  <td>Do laundry</td>
-                  <td>Complete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo">
-                  <td>Vacuum floor</td>
-                  <td>Incomplete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo is-completed">
-                  <td>Feed cat</td>
-                  <td>Complete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo">
-                  <td>Change light bulbs</td>
-                  <td>Incomplete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo is-completed">
-                  <td>Feed cat</td>
-                  <td>Complete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo">
-                  <td>Change light bulbs</td>
-                  <td>Incomplete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo is-completed">
-                  <td>Go to Store</td>
-                  <td>Completed</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-
-                <tr class="todo">
-                  <td>Fill gas tank</td>
-                  <td>Incomplete</td>
-                  <td class="todo-action">
-                    <button class="btn todo-btn">Change status</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <TodoList />
           </div>
         </section>
       </main>
