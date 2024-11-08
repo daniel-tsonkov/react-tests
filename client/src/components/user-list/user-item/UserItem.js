d1791d18320bc788bf3180226c2e7389b9ca6d6f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-11-08 09:38:10 +0200

    08112024

--- client/src/components/user-list/user-item/UserItem.js
+++ client/src/components/user-list/user-item/UserItem.js
@@ -37,7 +37,7 @@ export const UserItem = ({
             </path>
           </svg>
         </button>
-        <button className="btn info-btn" title="Info">
+        <button className="btn info-btn" title="Info" onClick={ }>
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info"
             className="svg-inline--fa fa-info" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="-150 0 512 612">
